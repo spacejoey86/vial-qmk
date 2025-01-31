2b94f4de54d01376e13631d3370a710a9d50db6f
= <message4dylan@gmail.com>
2025-01-31 22:40:54 +0000

    add ankey custom keyboard

---
+++ keyboards/ankey/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x19, 0xBD, 0x26, 0xF3, 0x0A, 0xF0, 0x9B, 0x1F}
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 0 }
---
+++ keyboards/ankey/keymaps/vial/keymap.c
@@ -0,0 +1,30 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *           ┌───┐                   ┌───┐
+      *           │GUI├───┐           ┌───┤Alt│
+      *           └───┤Bsp├───┐   ┌───┤Ent├───┘
+      *               └───┤   │   │   ├───┘
+      *                   └───┘   └───┘
+      */
+    [0] = LAYOUT_split_3x5_3(
+      KC_V, KC_B, KC_C, KC_X, KC_DELETE,   KC_Z,
+      KC_F, KC_G, KC_D, KC_S, KC_SPACE,    KC_A,
+      KC_R, KC_T, KC_E, KC_W, KC_LEFT_ALT, KC_Q,
+
+      KC_COMMA, KC_M, KC_DOT, KC_N, KC_SLASH,     KC_BACKSPACE,
+      KC_K,     KC_J, KC_L,   KC_H, KC_SEMICOLON, KC_ENTER,
+      KC_I,     KC_U, KC_O,   KC_Y, KC_P,         KC_LEFT_SHIFT
+    )
+};
